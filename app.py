105cc3f0fe78fb21942c30ff84f8d22b8c7bcdb7
lukas <lukaslinss98@gmail.com>
2023-06-13 14:49:07 +0200

    added froozen build file

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template
-from flask_frozen import Freezer
 
 app = Flask(__name__)
-
-freezer = Freezer(app)
 
 @app.route("/")
 def home():
     return render_template('home.html')
-
-if __name__ == '__main__':
-    freezer.freeze()
