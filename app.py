9fc4950197318be0245be707b41bd71057cc0cfb
lukas <lukaslinss98@gmail.com>
2023-06-13 14:46:46 +0200

    added frozen flask

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+
+freezer = Freezer(app)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+if __name__ == '__main__':
+    freezer.freeze()
